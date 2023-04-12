0180e20b1b7f675ab060938839cdb7fa0658518e
Dheerendra choudhary <dheerendrachoudhary340@gmail.com>
2023-04-12 21:11:13 +0530

    adding all required files

--- backend/src/main/kotlin/com/chess/backend/controller/HelloController.kt
+++ backend/src/main/kotlin/com/chess/backend/controller/HelloController.kt
@@ -1,5 +1,15 @@
 package com.chess.backend.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping("/chess/api")
 class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "This is chess backend"
+    }
 }
--- chess-ui/App.js
+++ chess-ui/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Game from "./src/Game";
 
 export default function App() {
    return (
       <View style={styles.container}>
-         <Text>Open up App.js to start working on your app!</Text>
-         <Text>Hello Chess Masters</Text>
+         <Game />
          <StatusBar style="auto" />
       </View>
    );
